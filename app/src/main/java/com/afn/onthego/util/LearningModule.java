8149b93d9837c75b50a43123440039dae5cc9fd5
Kyle Nicola <kyle.a.nicola@gmail.com>
2015-04-14 23:51:30 -0500

    separating out learning module

---
+++ app/src/main/java/com/afn/onthego/util/LearningModule.java
@@ -0,0 +1,58 @@
+package com.afn.onthego.util;
+
+/**
+ * Created by Kyle on 4/14/2015.
+ */
+public class LearningModule implements Comparable<LearningModule> {
+    public String name;
+    public String type;
+    public String data;
+    public String position;
+
+    public LearningModule()
+    {}
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+    public void setType(String type)
+    {
+        this.type = type;
+    }
+
+    public void setData(String data)
+    {
+        this.data = data;
+    }
+
+    public void setPosition(String position)
+    {
+        this.position = position;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public String getType()
+    {
+        return type;
+    }
+
+    public String getData()
+    {
+        return data;
+    }
+
+    public String getPosition()
+    {
+        return position;
+    }
+
+    public int compareTo(LearningModule another) {
+        return this.position.compareTo(another.position);
+    }
+}
