efd7dee5c762fedff124f63e23d71be5f8955e6e
Kyle Nicola <kyle.a.nicola@gmail.com>
2015-05-02 12:53:43 -0500

    removing dead code for Locations

--- app/src/main/java/com/afn/onthego/storage/KeyList.java
+++ app/src/main/java/com/afn/onthego/storage/KeyList.java
@@ -15,39 +15,6 @@ public class KeyList {
         public static final String CONNECT = "connect_activity";
     }
 
-    public static enum Locations {
-        AFN_ROSEWOOD(0), ARCH(1), BLACKLAND(2), CG_SENIOR_CTR(3),
-        RZ_NHOOD_CTR(4), SAUS_NHOOD_CTR(5), ST_JOHNS_CTR(6), TRINITY_CTR(7);
-
-        public static final LatLng[] LAT_LNGS = {
-                new LatLng(30.269932, -97.715876),
-                new LatLng(30.26787, -97.737584),
-                new LatLng(30.280945, -97.722188),
-                new LatLng(30.265868, -97.710984),
-                new LatLng(30.265391, -97.710594),
-                new LatLng(30.239574, -97.760286),
-                new LatLng(30.3328231, -97.6937014),
-                new LatLng(30.2685762, -97.7397648)
-        };
-
-        public static final String[] NAMES = {
-                "DeWitty Center",
-                "Austin Resource Center for the Homeless",
-                "Blackland Neighborhood Center",
-                "Conley-Guerrero Senior Activity Center",
-                "Rosewood Zaragosa Neighborhood Center",
-                "South Austin Neighborhood Center",
-                "St. John’s Neighborhood Center",
-                "Trinity Center"
-        };
-
-        public int index;
-
-        Locations(int i) {
-            index = i;
-        }
-    }
-
     public static class LearningModulesKeys {
         public static final String URL = "http://austinfreenet.pythonanywhere.com/";
         public static final String PREFS_LEARNING_MODULES = "learning_modules";
