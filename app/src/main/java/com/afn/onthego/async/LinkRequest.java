c0a4d36da76cdb1d778491988c573a0c1b928f2c
Kyle Nicola <kyle.a.nicola@gmail.com>
2015-05-02 11:42:17 -0500

    starting implementation of Links

---
+++ app/src/main/java/com/afn/onthego/async/LinkRequest.java
@@ -0,0 +1,83 @@
+package com.afn.onthego.async;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.afn.onthego.storage.KeyList;
+import com.afn.onthego.storage.Storage;
+import com.afn.onthego.util.LearningModules;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+/**
+ * Created by brian on 4/8/15.
+ */
+public class LinkRequest extends AsyncTask<Void, Void, String> {
+
+    private static final String LOG_TAG = "LinkRequest";
+    private LinkRequestListener callback;
+    private Context context;
+
+    // This is initiated in the MainActivity onCreate thread
+    public LinkRequest(Context context, LinkRequestListener callback)
+    {
+        this.context = context;
+        if(callback == null)
+            throw new IllegalArgumentException("Callback cannot be null!");
+        this.callback = callback;
+    }
+
+    @Override
+    protected String doInBackground(Void... params) {
+        //Obtain URL and build request from that
+        String url = KeyList.LinkKeys.URL;
+        OkHttpClient client = Storage.getInstance(context).getHttpClient();
+        Request request = new Request.Builder().url(url).build();
+
+        //Request data, handling failure cases
+        Response response;
+        String output = null;
+        try {
+            response = client.newCall(request).execute();
+            if(!response.isSuccessful())
+            {
+                Log.e(LOG_TAG, "Response not successful");
+                return null;
+            }
+            else if(!request.url().equals(response.request().url()))
+            {
+                Log.e(LOG_TAG, "Request URL is not the same as Response URL");
+                return null;
+            }
+
+
+            output = response.body().string();
+            response.body().close();
+        } catch (Exception e) {
+            Log.e(LOG_TAG, "Error getting data!", e);
+            return null;
+        }
+
+        return output;
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        if(result == null)
+            callback.onLinkRequestFailure();
+        else
+            callback.onLinkRequestSuccess(result);
+    }
+
+    public interface LinkRequestListener {
+        public void onLinkRequestSuccess(String s);
+        public void onLinkRequestFailure();
+    }
+}
---
+++ app/src/main/java/com/afn/onthego/util/Links.java
@@ -0,0 +1,14 @@
+package com.afn.onthego.util;
+
+/**
+ * Created by Kyle on 5/2/2015.
+ */
+public class Links {
+    public static String QUESTION_URL;
+    public static String VOLUNTEER_SIGN_UP_URL;
+    public static String VOLUNTEER_SIGN_IN_URL;
+    public static String DONATE_URL;
+    public static String AFN_URL;
+
+    public Links() {}
+}
